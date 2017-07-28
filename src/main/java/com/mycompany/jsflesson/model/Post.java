37b60ee72067a477856a5d5122c221b00f27f13f
Foyo89 <grzegorzfoik@gmail.com>
2017-07-29 01:27:10 +0200

    Utworzenie z klasy Post encję

--- src/main/java/com/mycompany/jsflesson/model/Post.java
+++ src/main/java/com/mycompany/jsflesson/model/Post.java
@@ -5,14 +5,59 @@
  */
 package com.mycompany.jsflesson.model;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author RENT
  */
-public class Post {
+
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable{
     
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    @Column
+    private Integer id;
+    @Column
     private String name;
+    @Column
     private String text;
+    @Column Date adddate;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Date getAdddate() {
+        return adddate;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setAdddate(Date adddate) {
+        this.adddate = adddate;
+    }
+    
+    
+    
 
     public String getName() {
         return name;
@@ -30,7 +75,30 @@ public class Post {
         this.text = text;
     }
     
-    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Post)) {
+            return false;
+        }
+        Post other = (Post) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.jsflesson.model.Post[ id=" + id + " ]";
+    }
    
     
 }
